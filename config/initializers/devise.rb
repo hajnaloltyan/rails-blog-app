9213dc98f83f599f024400e4316371adbbbef145
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-11-09 01:00:13 +0100

    Fix linter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,12 +9,6 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  # The secret key used by Devise. Devise uses this key to generate
-  # random tokens. Changing this key will render invalid all existing
-  # confirmation, reset password and unlock tokens in the database.
-  # Devise will use the `secret_key_base` as its `secret_key`
-  # by default. You can change it below and use your own secret key.
-
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
